d4bc7f2c8dd2469bf317f62fd839fdeeb5d6d0ef
GoncharovMain <goncharov.safe@gmail.com>
2022-07-03 03:26:53 +0300

    Init commit.

---
+++ Models/Message.cs
@@ -0,0 +1,9 @@
+namespace Models
+{
+    public class MessageViewModel
+    {
+        public string Text { get; set; }
+        public string Name { get; set; }
+        public DateTime PublishDate { get; set; }
+    }
+}
